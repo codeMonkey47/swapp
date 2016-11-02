e25a18eb79dc29d56014de70de362273438e4e3f
codeMonkey47 <vegesriharsha@gmail.com>
2016-11-01 21:41:33 -0500

    Commits
    1. css and html modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
   //encapsulation:ViewEncapsulation.None //Use only when you want to turn of encapsulation. This will propogate the top level li class component to children.
 })
 export class AppComponent implements OnInit{
-  title:string = 'Star Wars Trivia';
-
 
   ngOnInit(){
     //get initial data.
